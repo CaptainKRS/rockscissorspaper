11a823530723bcd6df980a3d09ec89ca04e407a7
Sherill <sherill.samuel@gmail.com>
2018-11-28 17:58:10 -0500

    Fixed expot default error yay!!!

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello, everyone loves puppies.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+class Game extends Component {
+  render () {
+    return(
+      <div className="style">
+        <div>Put components here</div>
+        <div>Who won?? </div>
+        <button type="button">Play the Game</button>
       </div>
-    );
+      )
   }
 }
 
-export default App;
+export default Game;
+
---
+++ src/PlayerCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlayerCard = (props) => {
+	return(
+		<div className="playerCard"></div>
+		)
+}
+
+export default PlayerCard;
